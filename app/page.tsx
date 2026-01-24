af978dee0fb6f4dc023f2c1b8110af919d286be4
bryanhohs <bryan@hohs.us>
2026-01-23 22:13:15 -0600

    update video tags

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <video className="videoTag" autoPlay loop muted id="bg-video" preload="auto">
+      <video preload="metadata" autoPlay loop muted id="bg-video">
         <source src="/hd_1920_1080_30fps.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
       </video>
       <div className="flex justify-center align-center w-full items-center h-160 mx-auto">
         <div className="w-full h-full flex flex-col justify-center items-center">
