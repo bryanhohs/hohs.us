c46b7dab10bb8adf0b8d6a92ba6e26e686d9518b
bryanhohs <bryan@hohs.us>
2026-01-25 03:11:48 -0600

    new icons

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
         <div className="w-full h-full flex flex-col justify-center items-center">
           <Image className="rounded-full" src="/profile.png" alt="Bryan C. Hohs" loading="eager" width={150} height={150} />
           <h1 className="text-center text-3xl font-black mt-5">Bryan C. Hohs</h1>
-          <h3 className="text-center text-md font-normal mt-3">CEO, Tech, Entrepreneur</h3>
-          <ul className="list-none flex flex-row space-x-10 mt-5">
+          <h3 className="text-center text-md font-normal mt-5">CEO, Tech, Entrepreneur</h3>
+          <ul className="list-none flex flex-row space-x-5 mt-5">
             <li>
               <a href="mailto:bryan.c@hohs.us" target="_blank">
                 <EnvelopeIcon className="size-10 text-black-500" />
@@ -35,7 +35,7 @@ export default function Home() {
               </a>
             </li>
           </ul>
-          <div className="text-sm pt-5">&copy; 2026 Bryan C. Hohs</div>
+          <div className="text-sm pt-5 mt-5">&copy; 2026 Bryan C. Hohs</div>
         </div>
       </div>
     </div>
