43baf1696f5190de20ecd63d09ecef69face6b89
bryanhohs <bryan@hohs.us>
2026-01-25 22:24:18 -0600

    update taillwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans, Noto_Sans_Mono } from "next/font/google";
 import "./globals.css";
+import "../styles/styles.css";
 
 const notoSans = Noto_Sans({
   variable: "--font-noto-sans",
