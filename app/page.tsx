abb6d2e083964c32f1017294927c5c127a949963
bryanhohs <bryan@hohs.us>
2026-01-25 18:51:07 -0600

    update icons

--- app/page.tsx
+++ app/page.tsx
@@ -14,24 +14,24 @@ export default function Home() {
           <h1 className="text-center text-3xl font-black mt-5">Bryan C. Hohs</h1>
           <h3 className="text-center text-md font-normal mt-5">CEO, Tech, Entrepreneur</h3>
           <ul className="list-none flex flex-row space-x-5 mt-5">
-            <li>
-              <a href="mailto:bryan.c@hohs.us" target="_self">
-                <EnvelopeIcon className="size-10 text-black-500" />
-              </a>
-            </li>
-            <li>
-              <a href="sms:13124399036" target="_self">
-                <ChatBubbleLeftRightIcon className="size-10 text-black-500" />
-              </a>
-            </li>
-            <li>
+            <li className="align-center items-center">
               <a href="tel:13124399036" target="_self">
                 <PhoneIcon className="size-10 text-black-500" />
               </a>
             </li>
-            <li>
+            <li className="align-center items-center">
+              <a href="sms:13124399036" target="_self">
+                <ChatBubbleLeftRightIcon className="size-10 text-black-500" />
+              </a>
+            </li>
+            <li className="align-center items-center">
               <a href="https://linktr.ee/bryanchohs" target="_blank">
                 <GlobeAmericasIcon className="size-10 text-black-500" />
+              </a>
+            </li>
+            <li className="align-center items-center">
+              <a href="mailto:bryan.c@hohs.us" target="_self">
+                <EnvelopeIcon className="size-10 text-black-500" />
               </a>
             </li>
           </ul>
