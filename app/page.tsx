2a2f6a03e515dae00dbc8beb107fd824c2274cb6
bryanhohs <bryan@hohs.us>
2026-01-29 01:30:27 -0600

    enable gpu

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { ChatBubbleLeftRightIcon, EnvelopeIcon, GlobeAmericasIcon, PhoneIcon } f
 export default function Hohs() {
   return (
     <div>
-      <video className="fixed top-0 left-0 w-full h-full object-cover z-[-1]" preload="metadata" autoPlay loop muted playsInline>
+      <video className="fixed top-0 left-0 w-full h-full object-cover z-[-1] transform-gpu" preload="metadata" autoPlay loop muted playsInline>
         <source src="/assets/videos/hd_1920_1080_30fps.mp4" type="video/mp4" />
           Your browser does not support the video tag.
       </video>
