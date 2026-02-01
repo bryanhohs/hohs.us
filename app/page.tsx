81c2c0fc3945705fec6055046e09bfee80d299b7
bryanhohs <bryan@hohs.us>
2026-01-31 21:19:24 -0600

    more css

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import { ChatBubbleLeftRightIcon, EnvelopeIcon, GlobeAmericasIcon, PhoneIcon } f
 export default function Hohs() {
   return (
     <div>
-      <div className="flex justify-center align-center items-center w-full h-150 mx-auto">
-        <div className="w-full h-full flex flex-col justify-center items-center">
+      <div className="flex justify-center align-center items-center w-full h-full mx-auto">
+        <div className="w-full h-160 flex flex-col justify-center items-center">
           <Image
             className="rounded-full"
             src={ProfileImage}
@@ -15,34 +15,34 @@ export default function Hohs() {
             width={150}
             height={150}
           ></Image>
-          <h1 className="text-center text-3xl font-black mt-5">Bryan C. Hohs</h1>
+          <h1 className="text-center text-3xl font-bold mt-5">Bryan C. Hohs</h1>
           <h3 className="text-center text-md font-normal mt-5">CEO, Tech, Entrepreneur</h3>
           <ul className="list-none flex flex-row space-x-5 mt-5">
             <li className="align-center items-center z-10">
               <a href="tel:13124399036" target="_self">
-                <PhoneIcon className="size-10 text-black-500" />
+                <PhoneIcon className="size-10 text-normal-500" />
               </a>
             </li>
             <li className="align-center items-center z-10">
               <a href="sms:13124399036" target="_self">
-                <ChatBubbleLeftRightIcon className="size-10 text-black-500" />
+                <ChatBubbleLeftRightIcon className="size-10 text-normal-500" />
               </a>
             </li>
             <li className="align-center items-center z-10">
               <a href="https://linktr.ee/bryanchohs" target="_blank">
-                <GlobeAmericasIcon className="size-10 text-black-500" />
+                <GlobeAmericasIcon className="size-10 text-normal-500" />
               </a>
             </li>
             <li className="align-center items-center z-10">
               <a href="mailto:bryan.c@hohs.us" target="_self">
-                <EnvelopeIcon className="size-10 text-black-500" />
+                <EnvelopeIcon className="size-10 text-normal-500" />
               </a>
             </li>
           </ul>
         </div>
       </div>
       <div className="text-center static bottom-0 text-sm mt-5">
-        <a className="text-black-500 hover:none" href="https://github.com/bryanhohs/hohs.us" target="_blank">
+        <a className="text-normal-500 hover:none" href="https://github.com/bryanhohs/hohs.us" target="_blank">
           &copy; 2026 Bryan C. Hohs
         </a>
       </div>
