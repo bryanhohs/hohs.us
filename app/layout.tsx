429d670978c27a7be90d79e273a213db4649a4d1
bryanhohs <bryan@hohs.us>
2026-01-31 20:59:40 -0600

    updated video component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Sans, Noto_Sans_Mono } from "next/font/google";
 import "./globals.css";
 import "./styles/styles.css";
-import VideoBackground from "./background";"./background";
+import VideoBackground from "./background";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/next';
 
