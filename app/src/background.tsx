0e98c372ebee09b4efab67f99d056744a4d7b16b
bryanhohs <bryan@hohs.us>
2026-02-24 23:24:35 -0600

    updates

--- app/src/background.tsx
+++ app/src/background.tsx
@@ -3,7 +3,7 @@
 export default function VideoBackground() {
   return (
     <div>
-      <video className="fixed top-0 left-0 w-full h-full object-cover z-[-1] transform-gpu" preload="metadata" autoPlay loop muted playsInline>
+      <video className="fixed top-0 left-0 w-full h-full object-cover z-[-1]" preload="metadata" autoPlay loop muted playsInline>
         <source src="/assets/videos/uhd_3840_2160_30fps.mp4" type="video/mp4" />
           Your browser does not support the video tag.
       </video>
