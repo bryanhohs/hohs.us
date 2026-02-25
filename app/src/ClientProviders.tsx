77a652082acee7c2aa8741c8789c4556d9e7fd69
bryanhohs <bryan@hohs.us>
2026-02-25 13:51:16 -0600

    updates

--- app/src/ClientProviders.tsx
+++ app/src/ClientProviders.tsx
@@ -1,23 +1,23 @@
 'use client';
 
 import React from 'react';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v16-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import { Paper, Box } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
-import EmotionRegistry from './EmotionRegistry';
 import theme from './theme';
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider theme={theme}>
-      <EmotionRegistry>
+    <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
           <Paper elevation={1} square sx={{ minHeight: '100vh', background: 'transparent' }}>
             <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
               {children}
             </Box>
           </Paper>
-        </EmotionRegistry>
-      </ThemeProvider>
+        </ThemeProvider>
+      </AppRouterCacheProvider>
   );
 }
