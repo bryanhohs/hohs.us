d7cd178cab0dffad000a54c6639e3064bee32a2f
bryanhohs <bryan@hohs.us>
2026-02-25 13:38:28 -0600

    updates

--- app/src/ClientProviders.tsx
+++ app/src/ClientProviders.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { Paper, Box } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import EmotionRegistry from './EmotionRegistry';
 import theme from './theme';
 
@@ -10,6 +11,7 @@ export default function ClientProviders({ children }: { children: React.ReactNod
   return (
     <ThemeProvider theme={theme}>
       <EmotionRegistry>
+        <CssBaseline />
         <Paper elevation={1} square sx={{ minHeight: '100vh', background: 'transparent' }}>
           <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
             {children}
