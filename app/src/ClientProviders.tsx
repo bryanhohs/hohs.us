da14ae2258a4a6e23a04428c40b1575566c05d58
bryanhohs <bryan@hohs.us>
2026-02-28 19:20:21 -0600

    updates

--- app/src/ClientProviders.tsx
+++ app/src/ClientProviders.tsx
@@ -12,7 +12,7 @@ export default function ClientProviders({ children }: { children: React.ReactNod
     <AppRouterCacheProvider options={{ enableCssLayer: true }}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-          <Container maxWidth="lg">
+          <Container maxWidth="lg" disableGutters sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
             <Paper elevation={1} square sx={{ minHeight: '100vh', background: 'transparent' }}>
               <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
                 {children}
