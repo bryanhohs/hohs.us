57068eee2113b4a24532643060876bce37d265d7
Bryan Hohs <bryan@hohs.us>
2026-03-08 18:40:27 +0000

    improved loader.tsx w/ mui typography

--- app/src/loader.tsx
+++ app/src/loader.tsx
@@ -1,11 +1,24 @@
 'use client'
 
+import { Stack, Typography } from '@mui/material'
+import { grey } from '@mui/material/colors'
 import CircularProgress from '@mui/material/CircularProgress'
 
 export default function Progress() {
   return (
-    <div className="flex flex-col items-center w-full h-full z-10 my-100">
-      <CircularProgress />
+    <div className="flex flex-col items-center w-full h-full z-10 my-75">
+      <div className="flex items-center justify-center">
+        <Stack>
+          <Typography variant="h3" gutterBottom color={grey[50]}>
+            Loading...
+          </Typography>
+        </Stack>
+      </div>
+      <div className="flex items-center justify-center">
+        <Stack>
+          <CircularProgress />
+        </Stack>
+      </div>
     </div>
   )
 }
