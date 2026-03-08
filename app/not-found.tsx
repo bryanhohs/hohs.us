51e0578c4feea2c734036b0c00e9013d668815e7
Bryan Hohs <bryan@hohs.us>
2026-03-08 11:12:12 +0000

    add mui to not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,15 @@
+'use client'
+
+import { Stack, Typography } from '@mui/material'
+import { grey } from '@mui/material/colors'
 export default function NotFound() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        minHeight: '50vh',
-      }}
-    >
-      <h1>Not Found</h1>
+    <div className='flex items-center justify-center my-50'>
+      <Stack>
+        <Typography variant="h3" gutterBottom color={grey[50]}>
+          Not Found
+        </Typography>
+      </Stack>
     </div>
   )
 }
